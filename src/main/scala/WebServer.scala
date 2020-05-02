d0a9431768c7127478d0711ad22e31e4177cbaa3
jay bhavsar <jay.bhavsar@laitmatus.com>
2020-05-02 18:22:59 +0530

    working on REST SetUP

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -1,6 +1,6 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.{ ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import scala.io.StdIn
@@ -8,29 +8,37 @@ import scala.io.StdIn
 object WebServer {
 
   def main(args: Array[String]): Unit = {
-
-    implicit val system: ActorSystem = ActorSystem("my-system")
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
 
     implicit val executionContext = system.dispatcher
 
 
-    val route = path("hello") {
-      get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say Hello to akka-http</h1>"))
-      }
+    val route = get {
+      concat(
+        pathSingleSlash{
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
+            "<html><body><h3>Hello World.</h3></body></html>"))
+        },
+        path("ping") {
+          complete("PONG!")
+        },
+        path("crash") {
+          sys.error("BOOM!")
+        }
+      )
     }
 
-    val bindingFuture = Http().bindAndHandle(route,"localhost", 8080)
+    //route will be implicitly converted to Flow using RouteResult.route2HandlerFlow
+    val bindingFutures = Http().bindAndHandle(route, "localhost", 8080)
 
-    println(s"Server online at http://localhost:8080/ \nPress RETURN TO STOP....")
+    println(s"Server online at http://localhost:8080/\n press RETURN to stop....")
 
-    StdIn.readLine()
+    StdIn.readLine()  //let it run until user presses return
 
-    bindingFuture
-      .flatMap(_.unbind())
-      .onComplete(_ => system.terminate())
+    bindingFutures
+      .flatMap(r => r.unbind()) //trigger unbinding from the port
+      .onComplete( r => system.terminate()) //and shutdown when done.
 
   }
-
 }
